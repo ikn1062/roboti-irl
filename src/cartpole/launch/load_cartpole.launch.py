2a808cbf89ba7b475c8996908628f8c6cf1202f0
Ishaan Narain <ishaannarain2022@u.northwestern.edu>
2023-04-07 11:55:05 -0500

    NOT WORKING - changed launch files to launch robot

--- src/cartpole/launch/load_cartpole.launch.py
+++ src/cartpole/launch/load_cartpole.launch.py
@@ -1,5 +1,7 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, Shutdown
+from launch.actions import SetLaunchConfiguration
+from launch.conditions import IfCondition
 from launch.substitutions import Command, LaunchConfiguration
 from launch.substitutions import PathJoinSubstitution
 from launch_ros.actions import Node
@@ -12,6 +14,15 @@ def generate_launch_description():
             'use_sim_time',
             default_value='false',
             description='Whether to use simulation clock [true/false]'),
+        DeclareLaunchArgument(
+            name='use_rviz',
+            default_value='true',
+            description='Whether to launch rviz2 [true/false]'),
+        DeclareLaunchArgument(
+            'r_frame',
+            default_value='world',
+            description='Name of the fixed frame in RVIZ.'
+        ),
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
@@ -26,5 +37,33 @@ def generate_launch_description():
             arguments=[PathJoinSubstitution([
                 FindPackageShare(package='cartpole'),
                 'urdf', 'xacro', 'cartpole_gazebo_urdf.urdf.xacro'])]
+        ),
+        Node(
+            package='joint_state_publisher',
+            executable='joint_state_publisher',
+            name='joint_state_publisher'),
+        DeclareLaunchArgument(
+            'rviz_config_name',
+            default_value='simulation.rviz',
+            description="Set path to configuration"),
+        SetLaunchConfiguration('rviz_config_file',
+                               PathJoinSubstitution([
+                                FindPackageShare(
+                                    package='cartpole'),
+                                'config',
+                                LaunchConfiguration('rviz_config_name')])),
+        Node(
+            condition=IfCondition(LaunchConfiguration('use_rviz')),
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            arguments=['-d',
+                       PathJoinSubstitution([
+                        FindPackageShare(package='cartpole'),
+                        'rviz', LaunchConfiguration('rviz_config_file')]
+                                            ),
+                       LaunchConfiguration('r_frame')],
+            on_exit=Shutdown()
         ),
     ])
