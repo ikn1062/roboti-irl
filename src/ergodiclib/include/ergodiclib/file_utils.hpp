8788ba2dbaeafcb8aaa7263f7bc07a14ce1bd1d7
Ishaan Narain <ishaannarain2022@u.northwestern.edu>
2023-05-22 16:29:17 -0500

    Fixing file utility functions

--- src/ergodiclib/include/ergodiclib/file_utils.hpp
+++ src/ergodiclib/include/ergodiclib/file_utils.hpp
@@ -1,7 +1,7 @@
 #ifndef FILE_UTIL_INCLUDE_GUARD_HPP
 #define FILE_UTIL_INCLUDE_GUARD_HPP
-/// \file
-/// \brief
+/// \file 
+/// \brief File utility functions for getting demonstrations from CSVs
 
 #include <iosfwd>
 #include <iostream>
@@ -19,11 +19,14 @@
 #include <armadillo>
 #endif
 
-/// \brief
-/// \param demonstration_file_list_path
-/// \param n_dimension
-/// \return
-std::vector<arma::mat> readDemonstrations(const std::string & demonstration_file_list_path, int n_dimension);
+
+namespace ergodiclib
+{
+/// \brief Reads Demonstrations from a given directory path
+/// \param demonstration_dir_path Directory path for demonstrations
+/// \param n_dimension Number of dimensions in dynamic system
+/// \return Vector of demonstration trajectories
+std::vector<arma::mat> readDemonstrations(const std::string & demonstration_dir_path, const int &n_dimension);
 
 /*
 /// \brief
@@ -33,12 +36,11 @@ std::vector<arma::mat> readDemonstrations(const std::string & demonstration_file
 std::vector< std::vector< std::vector<double> > > readDemonstrationsFileList(const std::string& demonstration_file_list_path, int n_dimension);
 */
 
-/// \brief
-/// \param csv_filepath
-/// \param n_dimension
-/// \return
-arma::mat readDemonstrationCSV(const std::string & csv_filepath, int n_dimension);
-
-bool compareStrings(const std::string & file_a, const std::string & file_b);
+/// \brief Read Demonstrations from a given CSV file
+/// \param csv_filepath File Path for demonstration csv 
+/// \param n_dimension Number of dimensions in dynamic system
+/// \return Matrix of demonstration trajectory
+arma::mat readDemonstrationCSV(const std::string & csv_filepath, const int &n_dimension);
+}
 
 #endif
--- src/ergodiclib/src/file_utils.cpp
+++ src/ergodiclib/src/file_utils.cpp
@@ -1,21 +1,42 @@
 #include <ergodiclib/file_utils.hpp>
 
 
+static bool compareStrings(const std::string & file_a, const std::string & file_b)
+{
+  // Define regular expression to match numeric part of string
+  std::regex num_regex("\\d+");
+
+  // Extract numeric part of each string
+  std::smatch a_match, b_match;
+  std::regex_search(file_a, a_match, num_regex);
+  std::regex_search(file_b, b_match, num_regex);
+  int a_num = std::stoi(a_match[0]);
+  int b_num = std::stoi(b_match[0]);
+
+  // Compare the numeric parts
+  return a_num < b_num;
+}
+
+
+namespace ergodiclib
+{
 std::vector<arma::mat> readDemonstrations(
-  const std::string & demonstration_folder_path, int n_dimension)
+  const std::string & demonstration_dir_path, const int &n_dimension)
 {
   std::vector<arma::mat> demonstrations;
 
+  // Get list of all files in the folder
   arma::mat demonstration_vec;
   std::vector<std::string> demonstration_list;
   std::string filename;
   for (const auto & dirEntry :
-    std::filesystem::recursive_directory_iterator(demonstration_folder_path))
+    std::filesystem::recursive_directory_iterator(demonstration_dir_path))
   {
     filename = dirEntry.path().generic_string();
     demonstration_list.push_back(filename);
   }
 
+  // Sort the folder by number and collect demonstrations
   std::sort(demonstration_list.begin(), demonstration_list.end(), compareStrings);
   for (const std::string & filename : demonstration_list) {
     std::cout << filename << std::endl;
@@ -44,23 +65,23 @@ std::vector<std::vector<std::vector<double>>> readDemonstrationsFileList(const s
 }
 */
 
-arma::mat readDemonstrationCSV(const std::string & csv_filepath, int n_dimension)
+arma::mat readDemonstrationCSV(const std::string & csv_filepath, const int &n_dimension)
 {
   arma::mat demonstration(0, 0, arma::fill::zeros);
   std::ifstream demonstration_file(csv_filepath); 
 
   std::vector<double> line_values;
   arma::vec col_vec;
-  std::string line;
+  std::string line, value;
   int demo_len;
+  double number;
   while (std::getline(demonstration_file, line)) {
     std::stringstream ss(line);
-    std::vector<double> line_values;
+    line_values.clear();
 
     for (int i = 0; i < n_dimension; i++) {
-      std::string value;
       std::getline(ss, value, ',');
-      double number = std::stod(value);
+      number = std::stod(value);
       line_values.push_back(number);
     }
     col_vec = arma::conv_to< arma::vec >::from(line_values);
@@ -72,19 +93,4 @@ arma::mat readDemonstrationCSV(const std::string & csv_filepath, int n_dimension
   demonstration_file.close();
   return demonstration;
 }
-
-bool compareStrings(const std::string & file_a, const std::string & file_b)
-{
-  // Define regular expression to match numeric part of string
-  std::regex num_regex("\\d+");
-
-  // Extract numeric part of each string
-  std::smatch a_match, b_match;
-  std::regex_search(file_a, a_match, num_regex);
-  std::regex_search(file_b, b_match, num_regex);
-  int a_num = std::stoi(a_match[0]);
-  int b_num = std::stoi(b_match[0]);
-
-  // Compare the numeric parts
-  return a_num < b_num;
 }
